5480ff2c32031349bc918b35a9fd01eb6bb6ad2b
arkenopterix <briegbk@yahoo.com>
2018-09-20 22:19:51 +0200

    Added tests of the UrEngine object

--- Tests/UrEngineTest.py
+++ Tests/UrEngineTest.py
@@ -22,14 +22,25 @@ class TestUrEngineMethods(unittest.TestCase):
         game2.board.placePawn(2, "B3")
         game2.board.placePawn(2, "C1")
         game2.board.placePawn(1, "C2")
+
         self.assertEqual(game2.scopeThroughBoard(4,"forward","A2",1),["MoveKO",""])
         self.assertEqual(game2.scopeThroughBoard(3,"forward","B1",2),['MoveOK', 'B4'])
         self.assertEqual(game2.scopeThroughBoard(3, "forward", "A2", 1), ['MoveReplace', 'C1'])
         self.assertEqual(game2.scopeThroughBoard(2, "forward", "C7", 2), ['MoveOK', 'B5'])
         self.assertEqual(game2.scopeThroughBoard(2, "forward", "C7", 1), ['MoveOK', 'A5'])
         self.assertEqual(game2.scopeThroughBoard(2,"backward", "B6",2),['MoveOK','C8'])
+        self.assertEqual(game2.scopeThroughBoard(2, "forward", "B5", 2), ['PawnSafe', ''])
 
         # self.assertEqual(game2.scopeThroughBoard(4, "forward", "A5", 1), ['MoveOK', 'A5'])
+    def testUrEngine_movePlayerPawn(self):
+        game3 = UrEngine()
+        game3.board.placePawn(1,"A2")
+        self.assertEqual(game3.movePlayerPawn(3,"A2",1),"MoveOK")
+        self.assertEqual(game3.board.getSquareState("C1"),1)
+        self.assertEqual(game3.movePlayerPawn(3, "A3", 1), "MoveKO")
+
+
+
 
 
 
